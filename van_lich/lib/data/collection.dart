59d32e512ac00659acb061700977df42e6a3057b
anhng195 <anhngw@gmail.com>
2023-05-27 22:08:38 +0700

    add content

--- van_lich/lib/data/collection.dart
+++ van_lich/lib/data/collection.dart
@@ -8,21 +8,29 @@ import 'package:van_lich/models/content.dart';
 // String thumbnail = '';
 // List<String> contentIds = [];
 final List<Collection> listOfCollection = [
-    Collection(
-      id: '001',
-      name: 'Coll1',
-      userId: '001',
-      description: 'Bo suu tap 1',
-      thumbnail: 'assets/images/trung-nhi.jpeg',
-      contentIds: ['001', '011'],
-    ),
-    Collection(
-      id: '002',
-      name: 'Coll2',
-      userId: '002',
-      description: 'Bo suu tap 2',
-      thumbnail: 'assets/images/ong-nam-cheo.jpeg',
-      contentIds: ['002', '022'],
-    ),
+  Collection(
+    id: '001',
+    name: 'Coll1',
+    userId: '001',
+    description: 'Huyền sử Việt Nam',
+    thumbnail: 'assets/images/tqt.jpeg',
+    contentIds: ['001', '011'],
+  ),
+  Collection(
+    id: '002',
+    name: 'Coll2',
+    userId: '002',
+    description: 'Văn hoá',
+    thumbnail: 'assets/images/chung.jpeg',
+    contentIds: ['002', '022'],
+  ),
 
+  Collection(
+    id: '002',
+    name: 'Coll2',
+    userId: '002',
+    description: 'Danh lam thắng cảnh',
+    thumbnail: 'assets/images/danh.jpeg',
+    contentIds: ['002', '022'],
+  ),
 ];
--- van_lich/lib/data/content.dart
+++ van_lich/lib/data/content.dart
@@ -18,59 +18,71 @@ import 'package:van_lich/models/content.dart';
 
 final List<Content> currentContent = [
   Content(
-      id: '001',
-      name: 'Trung tri',
-      userID: '001',
-      mainGraphicUrl: 'assets/images/trung-nhi.jpeg',
-      caption: 'Su that ve trung nhi',
-      content: 'Đất Mê Linh là đất bản bộ của các vua Hùng, kéo dài trên hai bờ sông Hồng từ phía trên của đỉnh tam giác châu Việt Trì (Phú Thọ) cho đến gần Hà Nội và trải rộng từ vùng núi Ba Vì (Hà Tây) sang vùng núi Tam Đảo (Vĩnh Phúc), tương đương với phần lớn các tỉnh Phú Thọ, Vĩnh Phúc và Hà Tây hiện nay. Sách Đại việt sử ký toàn thư chép: Trưng Trắc “nguyên là họ Lạc, con gái của Lạc tướng huyện Chu Diên (vùng lưu vực sông Đáy và mở sang cả sông Hồng bao gồm khu vực Hà tây, Hà Nội, Hà Nam hiện nay). Hai Bà Trưng, Trưng Trắc và Trưng Nhị, hai chị em sinh đôi là con gái Lạc tướng Mê Linh, đất bản bộ cũ của vua Hùng. Chồng Bà Trưng Trắc là Thi Sách, con trai lạc tướng Chu Diên. Lãnh thổ Mê Linh và Chu Diên liền cõi, hai gia đình lạc tướng là thông gia, khiến thanh thế của họ càng thêm mạnh, uy danh càng thêm lớn. Những năm đầu công nguyên, Cổ Loa với vị thế là một kinh đô cổ của đất nước đã sớm trở thành một trung tâm của các cuộc nổi dậy, đấu tranh giành độc lập của nhân dân, trong đố tiêu biểu nhất là cuộc khởi nghĩa của Hai Bà Trưng. Chính sách cai trị hà khắc, tàn bạo của nhà Đông Hán càng thôi thúc vợ chồng Trưng Trắc -Thi Sách hiệp mưu tính kế nổi dậy chống nhà Hán. Theo truyền thuyết và sử cũ, Thái Thú Tô Định đã giết chết Thi Sách trước khi cuộc khởi nghĩa nổ ra.',
-      createDate: DateTime(2023, 5, 26)
-
-
-  ),
-  Content(
       id: '011',
-      name: 'Trung trung',
+      name: 'Lệ Hải Bà Vương Triệu Thị Trinh',
       userID: '001',
-      mainGraphicUrl: 'assets/images/ong-nam-cheo.jpeg',
-      caption: 'Su that ve trung trung',
-      content: 'Hello tui la trung trung',
-      createDate: DateTime(2023, 5, 26)
-
+      mainGraphicUrl: 'assets/images/trieu2.jpg',
+      caption: 'Truyền thuyết về bà Triệu',
+      content: 'Trong lịch sử chống ngoại xâm của dân tộc, sau hai chị em bà Trưng Trắc, Trưng Nhị, lại thêm một lần nữa cho thấy phẩm chất, tài năng của những người con ưu tú đã làm vẻ vang cho nòi giống. Đấy chỉ mới nói riêng về phái nữ. Bà là em ông Triệu Quốc Đạt, người đất Trung Sơn thuộc quận Cửu Chân (nay là tỉnh Thanh Hoá), sống ở thời thuộc Ngô, cách ngày nay khoảng gần 1800 năm. Khicònnhỏ,bàcótênlàẨu.Tênấycóthểtừlúcsơsinhbàđãhứa hẹn một trang giai nhân tuyệt sắc sau này, nên cha mẹ đặt cho là theo cách dân gian để ma quỷ khỏi đến quấy quả? Cũng có thể, do lúc còn nhỏ bà đùa nghịch như con trai, lại bướng bỉnh hay gây gổ và đánh hỏng nhiều đồ đạc, nên mới gọi là Ẩu? Nhưng cũng có thể, chỉ đơn giản là cha mẹ thuận miệng mà gọi, chứ chẳng có hàm ý gì. Nhưng cho dù thế nào thì khi lớn lên, bà cũng vẫn là một trang giai nhân thực thụ. Sắc đẹp của bà không phải là sự lộng lẫy yêu kiều hay dáng vẻ yểu điệu thướt tha của những cung tần mỹ nữ ở nơi cung vua phủ chúa, mà là cái đẹp khoẻ mạnh, phóng khoáng của quê hương đồng nội, của đời sống nơi thôn dã. Trên gương mặt bà, tuy cũng là mắt phượng mày châu, nhưng sống mũi, khoé miệng lại toát lên vẻ cương nghị, quả cảm. Còn thân hình bà thì cao lớn, nở nang với đôi chân và đôi tay dài. Các sách vở thời trước ghi bà có vú dài ba thước chắc là đã nhấn mạnh đến vẻ đẹp nữ tính theo quan niệm của thời bấy giờ. Toàn bộ con người bà chứa đựng một nguồn sinh lực thật dồi dào và một phong thái hiên ngang đầy uy lực. Là con một vị hào trưởng tiếng tăm lừng lẫy trong vùng, nên từ nhỏ bà đã được luyện tập các môn võ nghệ. Càng lớn lên, thiên hướng “con nhà võ” của bà lại càng lộ rõ. Bà rất tinh thông các cách bài binh bố trận và tài sử dụng kiếm cung, đao thương. Tính tình của bà thì vẫn như xưa: ngang tàng và đầy khí phách, làm cho ngay cả đám mày râu cũng phải kính nể. Người anh Triệu Quốc Đạt của bà cũng là một người có tài năng xuất chúng, lại có đức độ và chí khí hơn người. Ông giao du rất rộng. Bạn bè và người tâm phúc vì thế mà cũng rất đông. Bốn người bạn là Vương Thiện, Lãnh Long, Bao Thúc và Tốn Thận, đều là những bậc kỳ tài và có đảm lược.',
+      createDate: DateTime(2023, 5, 26),
+      type: 'story'
 
   ),
   Content(
-      id: '002',
-      name: 'Ong nam cheo',
-      userID: '002',
-      mainGraphicUrl: 'assets/images/ong-nam-cheo.jpeg',
-      caption: 'Su that ve ong nam cheo',
-      content: 'Hello tui la ong nam cheo',
-      createDate: DateTime(2023, 5, 25)
-
-
+      id: '001',
+      name: 'Hai bà Trưng',
+      userID: '001',
+      mainGraphicUrl: 'assets/images/ba_trung.jpeg',
+      caption: 'Truyền thuyết Hai Bà Trưng',
+      content: 'Đất Mê Linh là đất bản bộ của các vua Hùng, kéo dài trên hai bờ sông Hồng từ phía trên của đỉnh tam giác châu Việt Trì (Phú Thọ) cho đến gần Hà Nội và trải rộng từ vùng núi Ba Vì (Hà Tây) sang vùng núi Tam Đảo (Vĩnh Phúc), tương đương với phần lớn các tỉnh Phú Thọ, Vĩnh Phúc và Hà Tây hiện nay. Sách Đại việt sử ký toàn thư chép: Trưng Trắc “nguyên là họ Lạc, con gái của Lạc tướng huyện Chu Diên (vùng lưu vực sông Đáy và mở sang cả sông Hồng bao gồm khu vực Hà tây, Hà Nội, Hà Nam hiện nay). Hai Bà Trưng, Trưng Trắc và Trưng Nhị, hai chị em sinh đôi là con gái Lạc tướng Mê Linh, đất bản bộ cũ của vua Hùng. Chồng Bà Trưng Trắc là Thi Sách, con trai lạc tướng Chu Diên. Lãnh thổ Mê Linh và Chu Diên liền cõi, hai gia đình lạc tướng là thông gia, khiến thanh thế của họ càng thêm mạnh, uy danh càng thêm lớn. Những năm đầu công nguyên, Cổ Loa với vị thế là một kinh đô cổ của đất nước đã sớm trở thành một trung tâm của các cuộc nổi dậy, đấu tranh giành độc lập của nhân dân, trong đố tiêu biểu nhất là cuộc khởi nghĩa của Hai Bà Trưng. Chính sách cai trị hà khắc, tàn bạo của nhà Đông Hán càng thôi thúc vợ chồng Trưng Trắc -Thi Sách hiệp mưu tính kế nổi dậy chống nhà Hán. Theo truyền thuyết và sử cũ, Thái Thú Tô Định đã giết chết Thi Sách trước khi cuộc khởi nghĩa nổ ra. Hành vi bạo ngược của Tô Định càng làm cho Bà Trưng Trắc cùng em gái là Trưng Nhị thêm quyết tâm tiến hành cuộc khởi nghĩa đánh đổ chính quyền đô hộ, rửa nợ nước, trả thù nhà, dựng lại cơ nghiệp xưa cho các Vua Hùng. Cửa sông Hát (Hát Môn, huyện Phúc Thọ, tỉnh Hà Tây) là nơi khởi phát công cuộc tụ nghĩa và khởi nghĩa. Thời điểm nổ ra cuộc khởi nghĩa là niên hiệu Kiến Vũ thứ 16 tức năm 40 SCN.',
+      createDate: DateTime(2023, 5, 26),
+      type: 'story'
   ),
   Content(
       id: '022',
-      name: 'Ong nam nam',
+      name: 'Trần Hưng Đạo',
       userID: '002',
-      mainGraphicUrl: 'assets/images/trung-nhi.jpeg',
-      caption: 'Su that ve ong nam nam',
-      content: 'Hello tui la ong nam nam',
-      createDate: DateTime(2023, 5, 25)
+      mainGraphicUrl: 'assets/images/tqt.jpeg',
+      caption: 'Hưng Đạo Đại Vương Trần Quốc Tuấn',
+      content: 'Hưng Đạo Đại Vương Trần Quốc Tuấn là vị danh tướng nhà Trần, lập nhiều chiến công hiển hách qua hai lần tiêu diệt quân Mông Nguyên xâm lược Đại Việt. Ngài mất vào năm 1300, dân kính ngưỡng tôn thờ gọi là Đức Thánh Trần. Ngài là hình tượng trung tâm của dòng tín ngưỡng Đức Thánh Trần rất phổ biến trong tâm thức người Việt; dân gian kể về Ngài qua những thần tích truyền thuyết kì ảo lạ thường. Người ta thường nhắc tới truyền thuyết về Đức Thánh Trần diệt giặc Phạm Nhan. Kể rằng thời xưa ở Đông Triều (Quảng Ninh), Tản Viên Sơn Thánh thấy luồng khói trắng bay từ núi Tây hóa thành tinh thuồng luồng xuống nhà người đàn bà nọ tư thông, nghĩ bụng ắt hẳn kẻ đó là ác quỷ đầu thai sẽ gây họa nhân gian. Bèn đem chuyện lên tâu với Ngọc Hoàng, Ngọc Đế hỏi văn võ bá quan thì có Thanh Tiên Đồng Tử tình nguyện hạ phàm giúp dân giúp nước. Ngọc Đế ưng thuận sai ban thần kiếm, cờ ấn, tam tài của Lão Tử, ngũ bảo của Thái Công rồi truyền Kim Đồng Ngọc Nữ hộ giá xe mây xuống nước Nam hạ phàm. Đời nhà Trần bấy giờ, có An Sinh Vương phu nhân, chồng là Trần Liễu anh ruột vua Trần Thái Tông, đêm đến nằm mộng thấy vị thiên thần tinh vàng tướng ngọc, tự xưng là Thanh Tiên Đồng Tử, theo lệnh Ngọc Hoàng xuống đầu thai. Vương Mẫu cấn thai, đến lúc lâm bồn có hào quang toả sáng rực cả nhà, hương thơm ngào ngạt, hạ sinh một cậu bé khôi ngô tuấn tú đặt tên là Trần Quốc Tuấn. Mới một tuổi Quốc Tuấn đã biết nói, sáu tuổi học hết kinh thư, biết nghĩ ra thế trận khôn lường. Cha ông là Trần Liễu biết con có tài thiên bẩm nên luôn rèn đúc để sau này con hay văn, giỏi võ.',
+      createDate: DateTime(2023, 5, 25),
+      type: 'story'
 
 
   ),
 
   Content(
-      id: '003',
-      name: 'Ba trieu',
+      id: '033',
+      name: 'Đại chiến thành Đa Bang - Phần 1',
       userID: '003',
-      mainGraphicUrl: 'assets/images/ba-trieu-01.png',
-      caption: 'Su that ve ba trieu',
-      content: 'Hello tui la ba trieu',
-      createDate: DateTime(2023, 5, 24)
+      mainGraphicUrl: 'assets/images/ho1.jpeg',
+      caption: 'Nhà Hồ thành lập',
+      content: 'assets/videos/demo1.mp4',
+      createDate: DateTime(2023, 5, 24),
+      type: 'video'
 
+  ),
+  Content(
+      id: '034',
+      name: 'Đại chiến thành Đa Bang - Phần 2',
+      userID: '003',
+      mainGraphicUrl: 'assets/images/ho2.jpeg',
+      caption: 'Âm mưu xâm lược',
+      content: 'assets/videos/demo2.mp4',
+      createDate: DateTime(2023, 5, 24),
+      type: 'video'
+
+  ),
+  Content(
+      id: '035',
+      name: 'Đại chiến thành Đa Bang - Phần 3',
+      userID: '003',
+      mainGraphicUrl: 'assets/images/ho3.jpeg',
+      caption: 'Quyết chiến',
+      content: 'assets/videos/demo3.mp4',
+      createDate: DateTime(2023, 5, 24),
+      type: 'video'
 
   ),
 ];
+
